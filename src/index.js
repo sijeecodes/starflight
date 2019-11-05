581000c2eeb76cc308887c1ad7690dbbcbc6c740
Simon <sijeecodes@gmail.com>
2019-11-05 12:11:31 +0900

    ADD 3Dmodel and animation, direction light and shadows, ground texture

--- src/index.js
+++ src/index.js
@@ -1,39 +1,77 @@
 import * as THREE from 'three';
+import { FBXLoader } from './libs/FBXLoader';
 
 var container, camera, scene, renderer, hemiLight, directLight;
 var ground;
 
+var mixer, clock = new THREE.Clock();
+
 init();
+animate();
 
 function init() {
   container = document.createElement( 'div' );
   document.body.appendChild( container );
 
   scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xa0a0a0 );
+  scene.fog = new THREE.Fog( 0xa0a0a0, 150, 300 );
 
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set( 0, 30, 30 );
-  camera.lookAt(scene.position);
+  camera.position.set( 0, 45, -40 );
+  camera.rotation.y = -0.5 * Math.PI;
+  camera.lookAt(0, 30, 0);
+  // x(+left/-right) y(+high/-low) z(+front/-rear)
 
-  hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.9 );
-  hemiLight.position.set( 0, 20, 0 );
+  hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 1.5 );
   scene.add( hemiLight );
 
-  var geometry = new THREE.PlaneGeometry( 60, 30, 1, 1 );
-  var material = new THREE.MeshLambertMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+  directLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
+  // directLight = new THREE.SpotLight( 0xffffff );
+  directLight.position.set( 100, 200, 100 );
+  directLight.castShadow = true;
+  directLight.shadow.mapSize.width = 5120;
+  directLight.shadow.mapSize.height = 5120;
+  directLight.shadow.camera.bottom = -200;
+  directLight.shadow.camera.left = -200;
+  directLight.shadow.camera.top = 80;
+  directLight.shadow.camera.right = 80;
+  // directLight.shadow.camera.near = 0.1;
+  // directLight.shadow.camera.far = 2000;
+  scene.add( directLight );
+
+  var geometry = new THREE.PlaneGeometry( 1500, 1500, 10, 10 );
+  var texture = new THREE.TextureLoader().load( 'models/grass.jpg' );
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 20, 20 );
+  var material = new THREE.MeshPhongMaterial( { map: texture } );
   ground = new THREE.Mesh( geometry, material );
   ground.rotation.x = -0.5 * Math.PI;
+  ground.receiveShadow = true;
+  ground.castShadow = false;
   scene.add( ground );
 
-  var axes = new THREE.AxesHelper(3);
-  scene.add( axes );
-  var gridXZ = new THREE.GridHelper(10, 5);
-  scene.add( gridXZ );
+  var loader = new FBXLoader();
+  loader.load( 'models/run.fbx', function ( object ) {
+    mixer = new THREE.AnimationMixer( object );
+    var action = mixer.clipAction( object.animations[ 0 ] );
+    action.play();
+    object.traverse( function ( child ) {
+      if ( child.isMesh ) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    } );
+    object.scale.set( 0.2, 0.2, 0.2 );
+    scene.add( object );
+  } );
 
   renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
-  // renderer.shadowMap.enabled = true;
-  renderer.render( scene, camera );
+  renderer.shadowMap.enabled = true;
+
   container.appendChild( renderer.domElement );
 
   window.addEventListener( 'resize', onWindowResize, false );
@@ -43,4 +81,16 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  var delta = clock.getDelta();
+
+  if ( mixer ) {
+    mixer.update( delta );
+  }
+
+  renderer.render( scene, camera );
 }
